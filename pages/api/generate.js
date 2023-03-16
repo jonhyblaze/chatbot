5f4adff958a82aab6eed3119a7b0ffcf54b57531
Blaze Jonhy <jonhy.blaze@gmail.com>
2023-03-16 18:11:36 +0200

    .env setup

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,8 +1,10 @@
 import { Configuration, OpenAIApi } from "openai"
+require('dotenv').config()
 
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 })
+
 
 const openai = new OpenAIApi(configuration)
 
